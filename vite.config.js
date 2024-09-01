00242b436d827ee51be6dba814eede5289e0ff4e
Jungmu <wjdan022@naver.com>
2024-09-01 20:41:08 +0900

    vite 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "@svgr/roolup";
+import svgr from "vite-plugin-svgr"; // vite-plugin-svgr을 사용
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(), // vite-plugin-svgr을 사용하는 경우
+  ],
   server: {
     port: 3000,
   },
